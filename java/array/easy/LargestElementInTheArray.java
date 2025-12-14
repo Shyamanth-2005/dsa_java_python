80b9da82594f255085fc2ea82776af8b79d8b9de
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-14 13:49:40 +0530

    initial code commit

--- java/array/easy/LargestElementInTheArray.java
+++ java/array/easy/LargestElementInTheArray.java
@@ -1,4 +1,3 @@
-
 public class LargestElementInTheArray{
   public static int largestElement(int[] arr) {
     int max = Integer.MIN_VALUE;
@@ -12,8 +11,10 @@ public class LargestElementInTheArray{
 
   public static void main(String[] args) {
     int[] arr1 = {2,5,1,3,0};
-    int[] arr2 = {8,10,5,7,9};
+    int[] arr2 = { 8, 10, 5, 7, 9 };
+    int[] arr3 = { 32, 23, 24, 21, 4 };
     System.out.println(largestElement(arr1));
     System.out.println(largestElement(arr2));
+    System.out.println(largestElement(arr3));
   }
 }
