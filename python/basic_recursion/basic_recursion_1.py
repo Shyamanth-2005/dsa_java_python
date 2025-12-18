ef410412d93f50759ac727127b1cea31f55a0554
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-18 14:57:41 +0530

    improved python version of basic recursion

--- python/basic_recursion/basic_recursion_1.py
+++ python/basic_recursion/basic_recursion_1.py
@@ -1,9 +1,26 @@
-def print_name(n):
-  if n <= 0 :
-    return " "
-  return "Shyamanth " + print_name(n-1)
+# def print_name(n):
+#   if n <= 0 :
+#     return " "
+#   return "Shyamanth " + print_name(n-1)
   
   
-if __name__ == '__main__':
-  n = 4
-  print(print_name(n))
+# if __name__ == '__main__':
+#   n = 4
+#   print(print_name(n))
+
+class Solution:
+  def print_name(self,name,count,N):
+    if count == N:
+      return 
+    print(name,end = " ")
+    return self.print_name(name,count+1,N)
+  
+def main():
+  sol = Solution()
+  name = "Shyamanth"
+  N = 23
+  sol.print_name(name,0,N)
+  
+  
+if __name__ == "__main__":
+  main()
