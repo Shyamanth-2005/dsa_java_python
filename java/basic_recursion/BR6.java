2e265f0046c9f10004bded1f730f51c5ee342061
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-23 09:57:36 +0530

    updated BR6.java file where reverse recursion can be done with just one variable

--- java/basic_recursion/BR6.java
+++ java/basic_recursion/BR6.java
@@ -1,4 +1,14 @@
 public class BR6 {
+  public static void reverse1(int[] arr, int n, int i) {
+    if (i >= n / 2) {
+      return;
+    }
+    int temp = arr[i];
+    arr[i] = arr[n - i - 1];
+    arr[n - i - 1] = temp;
+    reverse1(arr, n, i+1);
+    
+  }
   public static void reverse(int left, int right, int[] arr) {
     if (left >= right) {
       return;
@@ -15,6 +25,14 @@ public class BR6 {
     int right = arr.length - 1;
     reverse(left, right, arr);
     for (int num : arr) {
+      System.out.print(num + " ");
+    }
+    System.out.println();
+    int[] arr1 = { 1, 4, 5, 3, 6 };
+    int n = arr.length;
+    int i = 0;
+    reverse1(arr1, n, i);
+    for (int num : arr1) {
       System.out.print(num+" ");
     }
   }
