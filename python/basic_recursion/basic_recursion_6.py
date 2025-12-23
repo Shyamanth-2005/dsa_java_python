73dd930f6bc0e6d269f2af1e88e19c010812aa03
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-12-23 10:11:50 +0530

    updated basic_recursion_6.py for one pointer recursion

--- python/basic_recursion/basic_recursion_6.py
+++ python/basic_recursion/basic_recursion_6.py
@@ -1,3 +1,8 @@
+def reverse_1(i,n,arr):
+  if i >= n//2:
+    return
+  arr[i],arr[n-i-1] = arr[n-i-1],arr[i]
+  reverse_1(i+1,n,arr)
 def reverse(left,right,arr):
   if left >= right :
     return
@@ -10,7 +15,12 @@ def main():
   
   reverse(left,right,arr)
   print(arr)
-
+  print("\n")
+  arr = [1,4,5,3,6]
+  i = 0
+  n = len(arr)
+  reverse_1(i,n,arr)
+  print(arr)
 
 if __name__ == "__main__":
   main()
